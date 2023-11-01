38d106d544d5744ee7cb098e1bba2d227ceeaf77
kdm6859 <kdm6859@naver.com>
2023-11-01 17:18:09 +0900

    1

--- Assets/1.Script/Player.cs
+++ Assets/1.Script/Player.cs
@@ -7,19 +7,22 @@ public class Player : MonoBehaviour
 {
     [SerializeField] GameObject cameraOffset;
     [SerializeField] TextMeshProUGUI posText;
+    [SerializeField] GameObject enemy;
 
     // Start is called before the first frame update
     void Start()
     {
-        Screen.SetResolution(720, 1280, false);
+        //Screen.SetResolution(720, 1280, false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Screen.SetResolution(720, 1280, false);
+        //Screen.SetResolution(720, 1280, false);
         cameraOffset.transform.position = transform.localPosition;
-        transform.position = Vector3.zero;
-        posText.text = cameraOffset.transform.position + " " + transform.position;
+        //cameraOffset.transform.rotation = Quaternion.Euler(transform.localRotation.eulerAngles.x, transform.localRotation.eulerAngles.y, transform.localRotation.eulerAngles.z);
+        transform.localPosition = Vector3.zero;
+        //transform.localRotation = Quaternion.Euler(0, 0, 0);
+        posText.text = cameraOffset.transform.position + " " + transform.localPosition + "\n¿ÀÂ¡¾î : " + enemy.transform.position;
     }
 }
